7204ef38fc44dc5fbc2eb85656f152b99793f9a8
白滨 <306558437@qq.com>
2020-03-27 23:42:45 +0800

    35-制作水面

---
+++ Assets/Scripts/Object/Water.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //对游戏物体造成伤害TODO
+
+        //血量减少TODO
+        
+    }
+}
+
---
+++ Assets/Scripts/Other/OrderSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderSetting : MonoBehaviour
+{
+
+    MeshRenderer meshRenderer;
+    public int orderInLayer;
+
+    public void Start()
+    {
+        meshRenderer = transform.GetComponent<MeshRenderer>();
+        meshRenderer.sortingOrder = orderInLayer;
+    }
+}
+
