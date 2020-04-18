34a19f2135c869a3c1e0288e5821fbd344bbdf9c
白滨 <306558437@qq.com>
2020-04-18 20:17:20 +0800

    59-ChangeResetPos

---
+++ Assets/Scripts/Other/ChangerSpawnPos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangerSpawnPos : MonoBehaviour
+{
+    public Damage[] needChangeDamages;
+
+    public string TargetSpawn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == TagConst.Player)
+        {
+            for (int i = 0; i < needChangeDamages.Length; i++)
+            {
+                needChangeDamages[i].ResetPos = TargetSpawn;
+            }
+           
+        }
+    }
+}
